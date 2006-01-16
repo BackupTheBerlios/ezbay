dbb66bb189408cfb152c54d8e4fb79b939cce2d9
axlomoso <axlomoso>
2006-01-16 21:32:40 +0000

    Ceci est un test de dépose de projet dans le repository CVS de berlios.de

---
+++ HelloWorld/src/test/helloWorld/ejb/HelloWorldBean.java
@@ -0,0 +1,101 @@
+package test.helloWorld.ejb;
+
+import java.rmi.RemoteException;
+
+import javax.ejb.EJBException;
+import javax.ejb.SessionBean;
+import javax.ejb.SessionContext;
+
+import javax.ejb.CreateException;
+
+/**
+ * XDoclet-based session bean.  The class must be declared
+ * public according to the EJB specification.
+ *
+ * To generate the EJB related files to this EJB:
+ *		- Add Standard EJB module to XDoclet project properties
+ *		- Customize XDoclet configuration for your appserver
+ *		- Run XDoclet
+ *
+ * Below are the xdoclet-related tags needed for this EJB.
+ * 
+ * @ejb.bean name="HelloWorld"
+ *           display-name="Name for HelloWorld"
+ *           description="Description for HelloWorld"
+ *           jndi-name="ejb/HelloWorld"
+ *           type="Stateless"
+ *           view-type="both"
+ */
+public class HelloWorldBean implements SessionBean {
+
+	/** The session context */
+	private SessionContext context;
+
+	public HelloWorldBean() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Set the associated session context. The container calls this method 
+	 * after the instance creation.
+	 * 
+	 * The enterprise bean instance should store the reference to the context 
+	 * object in an instance variable.
+	 * 
+	 * This method is called with no transaction context. 
+	 * 
+	 * @throws EJBException Thrown if method fails due to system-level error.
+	 */
+	public void setSessionContext(SessionContext newContext)
+		throws EJBException {
+		context = newContext;
+	}
+
+	public void ejbRemove() throws EJBException, RemoteException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void ejbActivate() throws EJBException, RemoteException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void ejbPassivate() throws EJBException, RemoteException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * An ejbCreate method as required by the EJB specification.
+	 * 
+	 * The container calls the instance?s <code>ejbCreate</code> method whose
+	 * signature matches the signature of the <code>create</code> method invoked
+	 * by the client. The input parameters sent from the client are passed to
+	 * the <code>ejbCreate</code> method. Each session bean class must have at
+	 * least one <code>ejbCreate</code> method. The number and signatures
+	 * of a session bean?s <code>create</code> methods are specific to each 
+	 * session bean class.
+	 * 
+	 * @throws CreateException Thrown if method fails due to system-level error.
+	 * 
+	 * @ejb.create-method
+	 * 
+	 */
+	public void ejbCreate() throws CreateException {
+		// TODO Add ejbCreate method implementation
+	}
+
+	/**
+	 * An example business method
+	 *
+	 * @ejb.interface-method view-type = "both"
+	 * 
+	 * @throws EJBException Thrown if method fails due to system-level error.
+	 */
+	public String sayHello() throws EJBException {
+		return "Hello World !";
+	}
+
+}
