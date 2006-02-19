08c08a3237c5278e424c687e9bd4b638f311172a
axlomoso <axlomoso>
2006-02-19 15:29:50 +0000

    Axel : création d'un constructeur par défaut avec errMessage par défaut

--- ezBay/src/axlomoso/ezbay/exceptions/PseudoDejaUtiliseException.java
+++ ezBay/src/axlomoso/ezbay/exceptions/PseudoDejaUtiliseException.java
@@ -3,6 +3,10 @@ package axlomoso.ezbay.exceptions;
 public class PseudoDejaUtiliseException extends Exception {
 	String errMessage = "";
 	
+	public PseudoDejaUtiliseException(){
+		errMessage = "Le pseudo est déjà utilisé !";
+	}
+	
 	public PseudoDejaUtiliseException(String message){
 		errMessage = message;
 	}
